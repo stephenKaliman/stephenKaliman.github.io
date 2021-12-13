cd93e029e20a6202406793f12c1718253dd86311
Stephen Kelman <stephenrkelman@gmail.com>
2021-12-13 15:04:42 -0800

    testing php pls work

---
+++ utils/navbar.php
@@ -0,0 +1,17 @@
+
+<?php
+	function create_menu(string active_page)
+	{
+	  echo "<ul>";
+		echo "<li><a ".
+				(active_page=="home" ? "class=\"active\"":"").
+				" href=\"#home\">Home</a></li>";
+		echo "<li><a ".
+				(active_page=="about" ? "class=\"active\"":"").
+				" href=\"#about\">About Me</a></li>";
+		echo "<li><a ".
+				(active_page=="resume" ? "class=\"active\"":"").
+				" href=\"#resume\">Resume</a></li>";
+		echo "</ul>";
+	}	
+?>
